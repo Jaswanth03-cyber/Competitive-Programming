3549ab8a37f411f99b57f77a1ff99796d0dbc456
Jaswanth <87203223+Jaswanth03-cyber@users.noreply.github.com>
2022-07-21 11:53:43 +0530

    Add files via upload

--- Tree/binarysearchtree.cpp
+++ Tree/binarysearchtree.cpp
@@ -92,65 +92,82 @@ class binarysearchtree{
         }
     }
 // In order successor of a given node node with the min value of all the nodes whose keys are greater than the given key
-node* inorder_sucessor(node *n){
-    node *current = n->right ;
-    while(current->left != nullptr && current != nullptr){
-        current = current->left ;
-    }
-    return current ;
-}
-node *inorder_predecessor(node *n){
-    node *current = n->left ;
-    while(current->right != nullptr && current != nullptr){
-        current = current->right ;
-    }
-    return current ;
-}
-// deleting a node contains a total of three cases 
-node* Delete(node *n, int a){
-    if(n == nullptr){
-        return n ;
-    } 
-    // If the key to be deleted is smaller than the root's key then it lies in left subtree
-    if(n->data > a){
-        n->left = Delete(n->left,a) ;
-    }
-    // If the key to be deleted is greater than the root's key, then it lies in right subtree
-    else if(n->data < a){
-        n->right = Delete(n->right,a) ;
-    }
-    // if key is same as root's key, then This is the node to be deleted
-    else{
-        // node with no children case 1 
-        if(n->right == nullptr && n->left == nullptr){
-            return nullptr ;
+    node* inorder_sucessor(node *n){
+        node *current = n->right ;
+        while(current->left != nullptr && current != nullptr){
+            current = current->left ;
         }
+        return current ;
+    }
+    node *inorder_predecessor(node *n){
+        node *current = n->left ;
+        while(current->right != nullptr && current != nullptr){
+            current = current->right ;
+        }
+        return current ;
+    }
+// deleting a node contains a total of three cases 
+    node* Delete(node *n, int a){
+        if(n == nullptr){
+            return n ;
+        } 
+    // If the key to be deleted is smaller than the root's key then it lies in left subtree
+        if(n->data > a){
+            n->left = Delete(n->left,a) ;
+        }
+    // If the key to be deleted is greater than the root's key, then it lies in right subtree
+        else if(n->data < a){
+            n->right = Delete(n->right,a) ;
+        }
+    // if key is same as root's key, then This is the node to be deleted
+        else{
+        // node with no children case 1 
+            if(n->right == nullptr && n->left == nullptr){
+                return nullptr ;
+            }
     // Node with only one child case 2
         // has only left child 
-        else if(n->right == nullptr){
-            node *temp = n->left ;
-            delete n ;
-            return temp ;
-        }
+            else if(n->right == nullptr){
+                node *temp = n->left ;
+                delete n ;
+                return temp ;
+            }
         // has only right child
-        else if(n->left == nullptr){
-            node *temp = n->right ;
-            delete n ;
-            return temp ;
-        }
-        // has both the children case 3 
-        // here we try to get the in order sucessor of the node 
-        node *temp = inorder_sucessor(n) ;
-        // copying the data of inorder sucessor 
-        // cout<<temp->data<<endl ;
+            else if(n->left == nullptr){
+                node *temp = n->right ;
+                delete n ;
+                return temp ;
+            }
+        // has both the children case 3 we can do any of the following two cases both results a bst but different bsts 
+
+        
+        /*
+        Here if replacing with in order predecessor this is the code 
+        node *temp = inorder_predecessor(n) ;
+        cout<<temp->data<<endl ;
         n->data = temp->data ;
-        n->right = Delete(n->right, temp->data);
+        n->left = Delete(n->left, temp->data) ;
+        */
+        
+        // If we want to replace it by inorder successor this is the code 
+            node *temp = inorder_sucessor(n) ;
+        // copying the data of inorder sucessor 
+            cout<<temp->data<<endl ;
+            n->data = temp->data ;
+            n->right = Delete(n->right, temp->data);
+        }
+        return n ;
     }
-    return n ;
-}
 };
+
+
+/*
+Here in Delete node when we have two children for the node which we want to delete we replace it by in order sucessor and delete 
+it we can also do this we can replace it by in order predecessor and delete it for that implementation I will keep it in the comments
+
+*/
 int main(int argc, char **argv){
-    binarysearchtree T(30) ;
+    binarysearchtree T(50) ;
     node *n = T.root ;
     //node *m = T.insert(n,45) ;
     int a = 3 ;
@@ -158,29 +175,31 @@ int main(int argc, char **argv){
     cout<<b<<endl ;
     cout<<a<<endl ;
     node *m =  T.insert(n,35) ;
-    T.insert(n,90) ;
-    T.insert(n,27) ;
-    T.insert(n,29) ;
-    T.insert(n,1)  ;
+    T.insert(n,40) ;
+    T.insert(n,70) ;
+    T.insert(n,65) ;
+    T.insert(n,60)  ;
+    T.insert(n,80) ;
     T.inorder(n) ;
     cout<<endl ;
     T.postorder(n) ;
     cout<<endl ;
     T.preorder(n) ;
     cout<<endl ;
-    n = T.Delete(n,27) ;
+    n = T.Delete(n,70) ;
     T.inorder(n) ;
     cout<<endl ;
     T.postorder(n) ;
     cout<<endl ;
     T.preorder(n) ;
     cout<<endl ;
-    n = T.Delete(n,90) ;
+    /*n = T.Delete(n,90) ;
     T.inorder(n) ;
     cout<<endl ;
     T.postorder(n) ;
     cout<<endl ;
     T.preorder(n) ;
     cout<<endl ;
+    */
     return 0 ;
 }

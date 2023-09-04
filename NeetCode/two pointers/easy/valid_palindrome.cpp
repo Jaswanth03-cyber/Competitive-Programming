b55cce22c6c025188d03f590f41e4b8e41d6abef
Jaswanth <87203223+Jaswanth03-cyber@users.noreply.github.com>
2023-09-05 00:51:53 +0530

    Add files via upload

--- NeetCode/two pointers/easy/valid_palindrome.cpp
+++ NeetCode/two pointers/easy/valid_palindrome.cpp
@@ -1,48 +1,65 @@
 #include<bits/stdc++.h>
 using namespace std ;
 
+/*
+https://leetcode.com/problems/valid-palindrome/description/
+*/
+
+/*
+So if we can see the constraints we need to do the sum in O(N) or else O(LogN) as O(N) is not working we have to check it in O(LogN)
+
+So now we are having two pointers l = 0 and r = n-1 now, if both are alphanumeric checking if they are same and incrementing l and decreasing r
+but, now if one of them is not alphanumeric we should not change that l or r which is not alphanumeric as we have to ignore non alphanumeric
+characters, and remember we can not change a character in a string thats why I have created two character variables one and two after
+checking which value is not alphanumeric processing that 
+*/
+
 class Solution {
 public:
     bool isPalindrome(string s) {
-
-        string temp = "" ;
-        string two = "" ;
-        int n = s.size() ; 
-
-        for(int i = 0 ; i < n ; i++){
-            if((s[i] >= 'A') && (s[i] <= 'Z')){
-                s[i] = s[i] + 32 ;
-                temp = temp + s[i] ;
-            }
-            else if((s[i] >= 'a') && (s[i] <= 'z')){
-                temp = temp + s[i] ;
-            }
-            else if((s[i] >= '0') && (s[i] <= '9')){   
-                temp = temp + s[i] ;       
-            }
-            else{
-                continue ;
-            }
-            if((s[n-i-1] >= 'A') && (s[n-i-1] <= 'Z')){
-                s[n-i-1] = s[n-i-1] + 32 ;
-                two = two + s[n-i-1] ;
-            }
-            else if((s[n-i-1] >= 'a') && (s[n-i-1] <= 'z')){
-                two = two + s[n-i-1] ;
-            }
-            else if((s[n-1-i] >= '0') && (s[n-i-1] <= '9')){   
-                two = two + s[n-i-1] ;       
+        int n = s.length() ;
+        int l = 0 ;
+        int r = n-1 ;
+        bool ans = true ;
+        char temp = 32 ;
+        char one = 'a' ;
+        char two = 'b' ;
+        while(l <= r){
+            if(isalnum(s[l]) && isalnum(s[r])){
+                one = s[l] ;
+                two = s[r] ;
+                if(s[l] >= 'A' && s[l] <= 'Z'){
+                    one = s[l] + 32 ;
+                }
+                if(s[r] >= 'A' && s[r] <= 'Z'){
+                    two = s[r] + 32 ;
+                }
+                if(one != two){
+                    ans = false ;
+                    break ;
+                }
+                l++ ;
+                r-- ;
             }
             else{
+                if(isalnum(s[l])){
+                    if(!isalnum(s[r])){
+                        r-- ;
+                    }
+                }
+                else{
+                    if(!isalnum(s[r])){
+                        l++ ;
+                        r-- ;
+                    }
+                    else{
+                        l++ ;
+                    }
+                }
                 continue ;
             }
-        } 
-        cout<<temp<<endl ;
-        cout<<two<<endl ;
-        if(temp != two){
-            return false ;
         }
-        return true ;
+        return ans ;
     }
 };
 
